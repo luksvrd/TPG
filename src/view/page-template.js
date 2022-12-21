1b467f29fcba22fbcd8431f5ca38b92aa400af4b
luksvrd <ljvy9b@gmail.com>
2022-12-20 22:09:43 -0600

    styles link fix

--- src/view/page-template.js
+++ src/view/page-template.js
@@ -11,7 +11,7 @@ function createHTML(cardsHTML) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"/>
-      <link rel="stylsheet" href="./dist/style.css"/>
+      <link rel="stylesheet" href="style.css"/>
       <script src="https://kit.fontawesome.com/c502137733.js"></script>
       <title>Team Profile</title>
       </head>
