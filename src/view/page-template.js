15598cfbdb680d819a1cb19a3c8c9e116fa46177
luksvrd <ljvy9b@gmail.com>
2022-12-20 21:59:05 -0600

    fix css link

--- src/view/page-template.js
+++ src/view/page-template.js
@@ -10,11 +10,11 @@ function createHTML(cardsHTML) {
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <title>Team Profile</title>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-      <link re="stylesheet" href="style.css">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"/>
+      <link rel="stylsheet" href="./dist/style.css"/>
       <script src="https://kit.fontawesome.com/c502137733.js"></script>
-    </head>
+      <title>Team Profile</title>
+      </head>
     <body>
       <div class="container-fluid">
         <div class="row">
